16a9596268b37cc9dee9abf41e96d5f97ec6710a
Gareth Edwards <2792803E@student.gla.ac.uk>
2025-02-07 15:26:37 +0000

    Chap 4.2 Complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     
